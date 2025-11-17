8d8578dd3537533e1b90080d0cfe05ac3a9f2b8f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-17 13:12:13 +0800

    Setup & Hello World

---
+++ week-1/day1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DayOne
+{
+  class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Firs line of code in C#!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
